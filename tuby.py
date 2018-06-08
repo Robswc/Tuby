59a0dec5820a813af576ec8641695f1c65ab14e1
Robswc <robert.s.w.carroll@gmail.com>
2018-06-08 19:45:30 -0400

    Updated a lot, changed button color, added multithreading to all functions and worked on listbox, just need to clean up code in future

--- tuby.py
+++ tuby.py
@@ -1,4 +1,4 @@
-from threading import Thread
+import threading
 import subprocess
 import os
 from tkinter import *
@@ -6,40 +6,41 @@ from tkinter import messagebox
 from pytube import YouTube
 import youtubeQuery
 import sys
-from pydub import AudioSegment
+import shutil
 
-#def getAudioDownload(src):
-#    src = youtubeQuery.search(src)
-#    src = YouTube(src)
-#    print('Downloading...')
-#    src.streams.filter(only_audio=True).first().download(DOWNLOAD_FOLDER)
-#    print('Download Finished...')
+#This is a huge mess that needs to be cleaned up btw
+#will get to it """eventually"""(tm)
 
+DOWNLOAD_FOLDER = 'downloads/'
+TEMP_FOLDER = 'downloads/_tmp'
+FFMPEG_LOC = 'C:/Program Files/FFMPEG/ffmpeg.exe'
+RESULT = 'http://youtube.com/watch?v=9bZkp7q19f0'
+placeholder_text = 'search...'
 
 def getVideoDownload(src):
     src = youtubeQuery.search(src)
     src = YouTube(src)
     print('Downloading...')
-    src.streams.filter(only_audio=True).first().download(DOWNLOAD_FOLDER)
+    src.streams.filter(subtype='mp4').first().download(DOWNLOAD_FOLDER)
 
     print('Download Finished...')
     searchEntry.delete(0, 'end')
 
-
-DOWNLOAD_FOLDER = 'downloads/'
-RESULT = 'http://youtube.com/watch?v=9bZkp7q19f0'
-placeholder_text = 'search...'
-
-
 root = Tk()
 root.title("Tuby")
-root.geometry("360x512+0+0")
-root.iconbitmap("icon.ico")
+root.geometry("360x360+0+0")
+root.minsize(width=360, height=360)
+root.maxsize(width=900, height=360)
+#root.iconbitmap("icon.ico")
 root.configure(background='#DADADA')
 
+root.grid_columnconfigure(0, weight=1, uniform="group1")
+root.grid_columnconfigure(1, weight=1, uniform="group1")
+
 userSearch = StringVar()
-searchEntry = Entry(root,text="search...", textvariable=userSearch, font="Helvetica 22 bold", borderwidth="0")
-searchEntry.pack(fill=X, padx=(10,10), pady=(10,10))
+searchEntry = Entry(root,text="search...", textvariable=userSearch, font="Helvetica 22 bold", borderwidth="0", fg="#8F8F8F")
+searchEntry.grid(row=0, column=0, columnspan=2, ipadx=10, sticky="new", pady=(10,0))
+
 
 #class getAudioDownload(threading.Thread):
 #    def run(self):
@@ -50,7 +51,20 @@ searchEntry.pack(fill=X, padx=(10,10), pady=(10,10))
 #        src.streams.filter(only_audio=True).first().download(DOWNLOAD_FOLDER)
 #        print('Download Finished...')
 #
+def FFMPEGCheck():
+    if os.path.isfile('C:/Program Files/FFMPEG/ffmpeg.exe'):
+        print("True")
+    else:
+        print("C:/Program Files/FFMPEG/ffmpeg.exe")
+        messagebox.askyesno("Tuby", '"' + str(FFMPEG_LOC) + '"' + ' not found! FFMPEG is required for .mp3 downloads. Do you wish to download FFMPEG?')
 
+def startAudioDownload():
+    getAudioThread = threading.Thread(target=lambda: getAudioDownload((userSearch.get())))
+    getAudioThread.start()
+
+def startVideoDownload():
+    getVideoThread = threading.Thread(target=lambda: getVideoDownload((userSearch.get())))
+    getVideoThread.start()
 
 def getAudioDownload(src):
     downloadSuccess(str(src))
@@ -73,28 +87,34 @@ def openFolder():
      subprocess.Popen('explorer ' + str(cwd))
      print("Opening..." + str(cwd))
 
+def writeToDisk():
+    messagebox.showinfo("Tuby", 'Not yet available :( Write to Disk feature coming soon!')
 
 
-listbox = Listbox(root, bg="#C3C3C3", borderwidth="0", font="Helvetica 13 bold")
+listbox = Listbox(root, bg="#C3C3C3", borderwidth="0", font="Helvetica 9 bold")
+listbox.grid(row=1, column=0, columnspan=2, sticky="new")
 def listResults(src):
     for item in youtubeQuery.getResults(src):
         listbox.insert(END, item)
-listResults("gangnam style")
-listbox.pack(fill=X, padx=(10,10), pady=(0,10))
-downloadAudio = Button(root, borderwidth="0", bg="#548235", fg="white", font="Helvetica 13 bold", text="Audio", command=(lambda: getAudioDownload(userSearch.get())))
-downloadVideo = Button(root, borderwidth="0", bg="#2F5597", fg="white", font="Helvetica 13 bold", text="Video", command=(lambda: getVideoDownload(userSearch.get())))
-openFolderButton = Button(root, borderwidth="0", bg="#C3C3C3", text="Open", command=(lambda: openFolder()))
-writeToDiskButton = Button(root, borderwidth="0", bg="#C3C3C3", text="Write", command=(lambda: getVideoDownload(userSearch.get())))
-status = Label(root, text="Tuby // Created by Robert Carroll", bg="#DADADA", font="Helvetica 7", fg="grey", anchor=W)
+
+
+downloadVideo = Button(root, borderwidth="0", bg="#D82523", fg="white", font="Helvetica 13 bold", text="Download Video", command=(lambda: startVideoDownload()))
+downloadVideo.grid(row=2, column=0, columnspan=2, sticky="new")
+downloadAudio = Button(root, borderwidth="0", bg="#B11F1D", fg="white", font="Helvetica 13 bold", text="Download Audio", command=(lambda: startAudioDownload()))
+downloadAudio.grid(row=3, column=0, columnspan=2, sticky="new")
+openFolderButton = Button(root, borderwidth="0", bg="#C3C3C3", text="Open", font="Helvetica 13 bold", command=(lambda: openFolder()))
+openFolderButton.grid(row=5, column=0, sticky="nsew", pady=(10,10), padx=(10,5), rowspan=2)
+writeToDiskButton = Button(root, borderwidth="0", bg="#C3C3C3", text="Write", font="Helvetica 13 bold", command=(lambda: writeToDisk()))
+writeToDiskButton.grid(row=5, column=1, sticky="nsew", pady=(10,10), padx=(5,10), rowspan=2)
 
 #getAudioThread = getAudioDownload(name=userSearch.get())
 #getAudioThread.start()
 
-downloadAudio.pack(fill=X, padx=(10,10))
-downloadVideo.pack(fill=X, padx=(10,10))
-openFolderButton.pack(fill=X, padx=(10,5), pady=(10,10))
-writeToDiskButton.pack(fill=X, padx=(5,10), pady=(10,10))
-status.pack(side=BOTTOM, fill=X)
+
+#statusframe = Frame(root)
+#statusframe.pack(side=BOTTOM, fill=X)
+#status = Label(statusframe, text="Tuby // Created by Robert Carroll", bg="#DADADA", font="Helvetica 7", fg="grey", anchor=W)
+#status.pack(side=BOTTOM, expand=True)
 
 def clear_entry(event, searchEntry):
     searchEntry.delete(0, END)
@@ -102,9 +122,25 @@ def clear_entry(event, searchEntry):
 searchEntry.bind("<Button-1>", lambda event: clear_entry(event, searchEntry))
 
 searchEntry.insert(0, placeholder_text)
+
+def clear_temp():
+    try:
+        shutil.rmtree(TEMP_FOLDER)
+    except:
+        print("Error:")
+
+def create_downloads():
+    try:
+        os.makedirs(DOWNLOAD_FOLDER)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+create_downloads()
+
 root.mainloop()
 
-
+clear_temp()
 
 
 
--- youtubeQuery.py
+++ youtubeQuery.py
@@ -2,8 +2,8 @@ import urllib.request
 import urllib.parse
 import urllib
 import re
-import simplejson
-
+import lxml
+from lxml import etree
 
 def search(SEARCH):
     if "https://www.youtube.com/watch?v=" in SEARCH:
@@ -16,19 +16,22 @@ def search(SEARCH):
         return str("http://www.youtube.com/watch?v=" + search_results[0])
 
 def getResults(SEARCH):
+    results_return = []
     if "https://www.youtube.com/watch?v=" in SEARCH:
         return str(SEARCH)
     else:
         query_string = urllib.parse.urlencode({"search_query" : str(SEARCH)})
         html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
         search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-        print(search_results[0:5])
         for c in search_results[0:5]:
             id = str(c)
-            url = 'http://gdata.youtube.com/feeds/api/videos/%s?alt=json&v=2' % id
+            youtube = etree.HTML(urllib.request.urlopen("http://www.youtube.com/watch?v=" + (str(id))).read())
+            video_title = youtube.xpath("//span[@id='eow-title']/@title")
+            print(str(''.join(video_title)))
+            results_return.append((str(''.join(video_title))))
 
-            print(str(url))
-        return search_results[0:5]
+    return(results_return[0:5])
+
 
 
 
